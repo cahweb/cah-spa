72d4ddc9ab0de9e3c2aab0319bbda1b9178ef44f
Rachel Tran <cahweb@ucf.edu>
2019-11-26 11:08:07 -0500

    Init commit; added all previously untracked files.

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Custom functions for the SPA theme.
+ */
+
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/static/css/style.min.css' );
+    //Customized Style
+    wp_enqueue_style( 'child-style',  get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
+}
+
+?>
