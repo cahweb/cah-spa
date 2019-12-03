b6496a2dda38fb4064da1055b14b623f00ca9c85
Rachel Tran <cahweb@ucf.edu>
2019-12-03 10:43:39 -0500

    Added includes to custom functions and styles for events and developer functions.

--- functions.php
+++ functions.php
@@ -6,8 +6,18 @@
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/static/css/style.min.css' );
-    //Customized Style
+    
+    // Customized Style
     wp_enqueue_style( 'child-style',  get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
 }
+
+// Including child functions and their respective style sheets, which are separated into other files for organization.
+
+// Events
+include "functions/events.php";
+wp_enqueue_style('events-styles', get_stylesheet_directory_uri() . '/styles/events.css');
+
+// Developer functions for testing and debugging. Remove in production.
+include "functions/dev.php";
 
 ?>
